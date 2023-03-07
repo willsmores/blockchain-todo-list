a869ae3d0108689779eef0f6142031e8eeb8d8f5
Steve Willsmore <scwillsmore@gmail.com>
2023-03-07 16:39:55 -0500

    Add addTask function.

--- backend/contracts/TaskContract.sol
+++ backend/contracts/TaskContract.sol
@@ -5,8 +5,26 @@ contract TaskContract {
   event AddTask(address recipient, uint taskId)
   event DeleteTask(uint taskId, bool isDeleted)
 
+  // this defines the structure of each task object
+  // { id: 0, taskText: 'clean', isDeleted: false }
   struct Task {
-    
+    uint id;
+    string taskText;
+    bool isDeleted;
   }
 
+  // create an array that holds the task objects
+  Task[] private tasks;
+  // associates unique key for each task to a user address
+  // e.g. ----> {0: '0xsteve...7a'}, {1: '0xnikki...7a'}
+  mapping(uint256 => address) taskToOwner;
+
+  function addTask(string memory taskText, bool isDeleted) external {
+    // set the task id
+    uint taskId = tasks.length;
+    // append task to array
+    tasks.push(Task(taskId, taskText, isDeleted));
+    taskToOwner[taskId] = msg.sender; // gets wallet address of logged in user
+    emit AddTask(msg.sender, taskId)
+  }
 }
