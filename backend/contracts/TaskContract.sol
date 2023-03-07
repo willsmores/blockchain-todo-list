688f6bdaf095e4088e6d79a73410dd226e369e66
Steve Willsmore <scwillsmore@gmail.com>
2023-03-07 16:18:23 -0500

    Scaffold new contract.

---
+++ backend/contracts/TaskContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+contract TaskContract {
+  event AddTask(address recipient, uint taskId)
+  event DeleteTask(uint taskId, bool isDeleted)
+
+  struct Task {
+    
+  }
+
+}
