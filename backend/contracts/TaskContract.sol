92b6ffa7015ec5348343aa23ddf70da3ec2dcd10
Steve Willsmore <scwillsmore@gmail.com>
2023-03-10 11:21:01 -0500

    Add getMyTasks function.

--- backend/contracts/TaskContract.sol
+++ backend/contracts/TaskContract.sol
@@ -25,11 +25,30 @@ contract TaskContract {
     // append task to array
     tasks.push(Task(taskId, taskText, isDeleted));
     taskToOwner[taskId] = msg.sender; // gets wallet address of logged in user
-    emit AddTask(msg.sender, taskId)
+    emit AddTask(msg.sender, taskId);
   }
 
   // get tasks for logged in user that are not deleted
   function getMyTasks() external view returns (Task[] memory) {
-    temporary = new Task[](tasks.length);
+    // create temporary array
+    Task[] memory temporary = new Task[](tasks.length);
+    uint counter = 0;
+
+    // loop through Tasks and add to 'temporary'
+    // if tasks is for logged in user and task is not deleted
+    for (unit i = 0; i < tasks.length; i++) {
+      if (taskToOwner[i] == msg.sender && tasks[i].isDeleted == false) {
+        temporary[counter] = tasks[i]; // common way to 'push' in Solidity
+        counter++;
+      }
+    }
+    Task[] memory result = new Task[](counter);
+
+    for (uint i = 0; i < counter; i++) {
+      result[i] = temporary[i];
+    }
+
+    return result;
+
   }
 }
