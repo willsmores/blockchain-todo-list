3f8a18074879e36d65b332dcd86c64e821b44518
Steve Willsmore <scwillsmore@gmail.com>
2023-03-15 14:39:41 -0400

    Add connectWallet functionality.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,10 @@
-import WrongNetworkMessage from '../components/WrongNetworkMessage'
-import ConnectWalletButton from '../components/ConnectWalletButton'
-import TodoList from '../components/TodoList'
+import WrongNetworkMessage from "../components/WrongNetworkMessage";
+import ConnectWalletButton from "../components/ConnectWalletButton";
+import TodoList from "../components/TodoList";
+import TaskAbi from "../../backend/build/contracts/TaskContract.json";
+import { TaskContractAddress } from "../config";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 /* 
 const tasks = [
@@ -11,32 +15,59 @@ const tasks = [
 */
 
 export default function Home() {
-
   // Calls Metamask to connect wallet on clicking Connect Wallet button
   const connectWallet = async () => {
+    const [correctNetwork, setCorrectNetwork] = useState(false);
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+    const [currentAccount, setCurrentAccount] = useState("");
 
-  }
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("MetaMask not detected.");
+        return;
+      }
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      console.log("Connected to chain:", chainId);
+
+      const goerliChainId = "0x4";
+      if (chainId !== goerliChainId) {
+        alert("You are not connected to the Goerli testnet!");
+        setCorrectNetwork(false);
+      } else {
+        setCorrectNetwork(true);
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Found account", accounts[0]);
+      setIsUserLoggedIn(true);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // Just gets all the tasks from the contract
-  const getAllTasks = async () => {
-
-  }
+  const getAllTasks = async () => {};
 
   // Add tasks from front-end onto the blockchain
-  const addTask = async e => {
-
-  }
+  const addTask = async (e) => {};
 
   // Remove tasks from front-end by filtering it out on our "back-end" / blockchain smart contract
-  const deleteTask = key => async () => {
-
-  }
+  const deleteTask = (key) => async () => {};
 
   return (
-    <div className='bg-[#97b5fe] h-screen w-screen flex justify-center py-6'>
-      {!'is user not logged in?' ? <ConnectWalletButton /> :
-        'is this the correct network?' ? <TodoList /> : <WrongNetworkMessage />}
+    <div className="bg-[#97b5fe] h-screen w-screen flex justify-center py-6">
+      {!"is user not logged in?" ? (
+        <ConnectWalletButton />
+      ) : "is this the correct network?" ? (
+        <TodoList />
+      ) : (
+        <WrongNetworkMessage />
+      )}
     </div>
-  )
+  );
 }
-
